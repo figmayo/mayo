8f2de4a98d6478b95dc219738958bbabfb21e782
Mike Miller <mike@mikemiller.uk>
2024-08-27 18:47:21 +0200

    Tidy up unused code

--- src/cli/commands/reset.ts
+++ src/cli/commands/reset.ts
@@ -1,16 +1,14 @@
+import { execSync } from "child_process";
 import { GEN_PATH } from "../../constants";
 import { GluegunCommand } from "gluegun";
 import { green, red } from "kleur";
 
 const command: GluegunCommand = {
   name: "reset",
-  run: async ({ print, filesystem, build }) => {
+  run: async ({ print, filesystem }) => {
     try {
       const sourceDir = filesystem.path(__dirname, "..", "..", "cli/templates");
       const targetDir = filesystem.path(__dirname, "..", "..", "..", GEN_PATH);
-
-      // Ensure the target directory exists
-      filesystem.dir(targetDir);
 
       // Define the files you want to copy
       const filesToCopy = ["data.json", "types.ts"];
@@ -21,8 +19,7 @@ const command: GluegunCommand = {
         const targetFile = filesystem.path(targetDir, file);
         filesystem.copy(sourceFile, targetFile, { overwrite: true });
       });
-
-      build.compileProject("../../tsconfig.lib.json");
+      execSync(`npm run compile:lib`, { stdio: "ignore" });
       print.success(green("Reset complete!"));
     } catch (error) {
       print.error(red("An error occurred while resetting the files."));
--- src/cli/extensions/cli-extension.ts
+++ src/cli/extensions/cli-extension.ts
@@ -1,9 +1,7 @@
 import { GluegunToolbox } from "gluegun";
 import * as typegen from "../typegen";
-import * as build from "../build";
 // add your CLI-specific functionality here, which will then be accessible
 // to your commands
 module.exports = (toolbox: GluegunToolbox) => {
   toolbox.typegen = typegen;
-  toolbox.build = build;
 };
